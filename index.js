ecc442733dda39361760df2b327590e0f68ee3dd
Sarah Jacob <smjacob91@gmail.com>
2018-06-08 19:12:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,11 +26,4 @@ function appendKitten(name) {
   return [...kitten, name]
 }
 
-describe('appendKitten(name)', function() {
-    it('appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(appendKitten("Broom")).toEqual(["Milo", "Otis", "Garfield", "Broom"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
 
