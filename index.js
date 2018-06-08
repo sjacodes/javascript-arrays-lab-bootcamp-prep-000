cbb1b00da27af0d21f8cb19dc4a9d584c0e864ed
Sarah Jacob <smjacob91@gmail.com>
2018-06-08 18:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
-
+function destructivelyAppendKitten(name) {
+  
+}
 
 describe('destructivelyAppendKitten(name)', function() {
     it('appends a kitten to the end of the kittens array', function() {
