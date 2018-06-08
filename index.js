373e7c87003630366cec8958c108469a2b3d3a2c
Sarah Jacob <smjacob91@gmail.com>
2018-06-08 19:15:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,13 +30,10 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 
-function removeLastKitten()
+function removeLastKitten() {
+  kittens.slice()
+  return kittens
+}
 
- describe('removeLastKitten()', function() {
-    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
 
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
 
